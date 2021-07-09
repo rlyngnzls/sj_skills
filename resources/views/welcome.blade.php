1bfb112a4a3d6c08eebcdfc8c0ee75e8a7a7d288
Rolyn Gonzales <83931408+rlyngnzls@users.noreply.github.com>
2021-07-09 14:12:53 +0800

    Update Navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center">
+    <div class="d-flex align-items-center container" style="max-width: 90%;">
 
       <div class="logo mr-auto">
         {{-- <h1><a href="index.html">San Juan</a></h1> --}}
@@ -46,39 +46,46 @@
         <a href="index.html"><img src="{{asset('/img/makabago.png')}}" alt="" class="img-fluid"></a>
       </div>
 
-      <nav class="nav-menu d-none d-lg-block">
+      <nav class="nav-menu d-none d-lg-block ">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-          <li class="drop-down"><a href="">About</a>
+          <li class="active drop-down"><a>News</a>
             <ul>
-              <li><a href="#about">About Us</a></li>
-              <li><a href="#team">Team</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Dow</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
+              <li><a href="#">Articles</a></li>
+              <li><a href="#">Announcements</a></li>
             </ul>
           </li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
-          <li><a href="#contact">Contact Us</a></li>
-
+          <li class="drop-down"><a>Programs</a>
+            <ul>
+              <li><a href="#">Caregiving</a></li>
+              <li><a href="#">Baking</a></li>
+              <li><a href="#">Electronics</a></li>
+              <li><a href="#">Welding</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a>Class Schedules</a>
+            <ul>
+              <li><a href="#">January</a></li>
+              <li><a href="#">February</a></li>
+              <li><a href="#">March</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Career/Opportunities</a></li>
+          <li class="drop-down"><a>Gallery</a>
+            <ul>
+              <li><a href="#">Photo</a></li>
+              <li><a href="#">Video</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="">About Us</a>
+            <ul>
+              <li><a href="#">About the Office</a></li>
+              <li><a href="#">Mission & Vision</a></li>
+              <li><a href="#">Organizational Chart</a></li>
+              <li><a href="#">FAQ</a></li>
+            </ul>
+          </li>
         </ul>
       </nav><!-- .nav-menu -->
-
-      <div class="header-social-links">
-        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
-      </div>
-
     </div>
   </header><!-- End Header -->
 
