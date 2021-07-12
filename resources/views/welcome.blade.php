f6da85f614c51eae7a0ee0ce89a173ea428e68cf
harry-26 <85989001+harry-26@users.noreply.github.com>
2021-07-12 16:11:18 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -140,7 +140,7 @@
               <h4><a href="">Welding</a></h4>
               <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui official</p>
             </div>
-          </div>          
+          </div>
 
         </div>
 
@@ -172,7 +172,7 @@
               <li><i class="bx bx-chevron-right"></i> <a href="#">Caregiving</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Baking</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Electronics</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Welding</a></li>               
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Welding</a></li>
             </ul>
           </div>
 
@@ -197,7 +197,7 @@
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>       
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
       </div>
     </div>
   </footer><!-- End Footer -->
@@ -221,3 +221,22 @@
 </body>
 
 </html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+$(window).scroll(function() {
+          var $height = $(window).scrollTop();
+          $("#header").css("background","#fff");
+        if($height > 50) {
+              $("#header").css("background","#314096");
+              $(".nav-menu a").css("color","#fff");
+              $(".drop-down li a").css("color","inherit");
+          } else {
+              $("#header").css("background","inherit");
+              $(".nav-menu a").css("color","inherit");
+
+
+          }
+      });
+</script>
