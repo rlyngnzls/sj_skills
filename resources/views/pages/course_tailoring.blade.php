e9e47d7ecd626c83d006340e4a9de65cdd231fe6
kylegazacutie02 <87346837+kylegazacutie02@users.noreply.github.com>
2021-07-14 09:26:15 +0800

    ADDED COURSE

---
+++ resources/views/pages/course_tailoring.blade.php
@@ -0,0 +1,222 @@
+@extends('layout.app')
+@section('content')
+
+<!-- ======= Hero Section ======= -->
+  <section id="baking" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="container text-center text-md-left" data-aos="fade-up">
+      <h1>COURSE: <span>TAILORING</span></h1>
+      <h2>Be one of San Juan's talented personalities!</h2>
+      <a href="#about" class="btn-get-started scrollto">REGISTER NOW</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Offered Programs Section ======= -->
+    <section id="what-we-do" class="what-we-do">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>PROGRAM OUTLINE</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-lg-6">
+              <img src="{{asset('/img/aboutbaking.jpg')}}" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0">
+              <h3>BAKING</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <ul>
+                <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              </ul>
+              <div class="row icon-boxes">
+                <div class="col-md-6">
+                  <i class="bx bx-cake"></i>
+                  <h4>Corporis voluptates sit</h4>
+                  <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                  <i class="bx bx-dish"></i>
+                  <h4>Ullamco laboris nisi</h4>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section><!-- End About Section -->
+
+      </div>
+    </section><!-- Offered Programs Section -->
+     <!-- ======= Portfolio Section ======= -->
+     <section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>PORTFOLIO</h2>
+          <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">New</li>
+              <li data-filter=".filter-card">Old</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-1.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-1.jpg')}}" data-gall="portfolioGallery" class="link-preview venobox" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">App 1</a></h4>
+                <p>App</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-2.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-2.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Web 3</a></h4>
+                <p>Web</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-3.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-3.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">App 2</a></h4>
+                <p>App</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-4.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-4.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Card 2</a></h4>
+                <p>Card</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-5.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-5.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Web 2</a></h4>
+                <p>Web</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-6.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-6.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">App 3</a></h4>
+                <p>App</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-7.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-7.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Card 1</a></h4>
+                <p>Card</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp" data-wow-delay="0.1s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-8.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-8.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Card 3</a></h4>
+                <p>Card</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{asset('/img/portfolio/portfolio-9.jpg')}}" class="img-fluid" alt="">
+                <a href="{{asset('/img/portfolio/portfolio-9.jpg')}}" class="link-preview venobox" data-gall="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Web 1</a></h4>
+                <p>Web</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+  </main><!-- End #main -->
+  @endsection
