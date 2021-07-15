d394bbbcf3fd3ea60e7817f5d2e29d7d3e143d19
harry-26 <85989001+harry-26@users.noreply.github.com>
2021-07-15 10:24:48 +0800

    banner slider

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,17 +1,66 @@
 @extends('layout.app')
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 
 <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+  <!-- <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="container text-center text-md-left" data-aos="fade-up">
       <h1>WELCOME TO <span>SAN JUAN SKILLS</span></h1>
       <h2>Be one of San Juan's talented personalities!</h2>
       <a href="#about" class="btn-get-started scrollto">REGISTER NOW</a>
     </div>
-  </section><!-- End Hero -->
+  </section><!-- End Hero --> -->
 
   <main id="main">
 
+    <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('/img/care.jpg')}}" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('/img/slidebake.jpg')}}" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('/img/slideelec.jpg')}}" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('/img/slidewelding.jpg')}}" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
     <!-- ======= Offered Programs Section ======= -->
     <section id="what-we-do" class="what-we-do">
       <div class="container">
